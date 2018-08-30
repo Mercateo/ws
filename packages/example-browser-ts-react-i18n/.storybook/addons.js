ec8caf09ba49fa24bf05822dbaff10c89152a846
Philipp Zins <philipp.zins@mercateo.com>
2018-08-30 12:27:38 +0200

    moved storybook configs

--- packages/example-browser-ts-react-i18n/.storybook/addons.js
+++ packages/example-browser-ts-react-i18n/.storybook/addons.js
@@ -1,6 +1,1 @@
-import '@storybook/addon-knobs/register';
-import '@storybook/addon-viewport/register';
-import '@storybook/addon-actions/register';
-import '@storybook/addon-backgrounds/register';
-import '@storybook/addon-a11y/register';
-import '@storybook/addon-storysource/register';
+import '@mercateo/ws/dist/storybook-addons';
--- packages/example-browser-ts-react-i18n/.storybook/config.js
+++ packages/example-browser-ts-react-i18n/.storybook/config.js
@@ -1,38 +1,5 @@
-import 'babel-polyfill';
-import { configure, addDecorator } from '@storybook/react';
-import { withKnobs } from '@storybook/addon-knobs';
-import { withBackgrounds } from '@storybook/addon-backgrounds';
-import { checkA11y } from '@storybook/addon-a11y';
-
-addDecorator(checkA11y);
-addDecorator(withKnobs);
-addDecorator(
-  withBackgrounds([
-    {
-      name: 'default',
-      value: '#f3f2f1',
-      default: true
-    },
-    {
-      name: 'white',
-      value: '#ffffff'
-    },
-    {
-      name: 'dark',
-      value: '#4a4a4a'
-    },
-    {
-      name: 'transparent',
-      value:
-        'url(data:image/gif;base64,R0lGODlhEAAQAIAAAOXl5f///yH5BAAAAAAALAAAAAAQABAAAAIfjG+gq4jM3IFLJgpswNly/XkcBpIiVaInlLJr9FZWAQA7)'
-    },
-    {
-      name: 'grid',
-      value:
-        'url(data:image/gif;base64,R0lGODlhEAAQAJEAAP///8iikfDl3wAAACH5BAAAAAAALAAAAAAQABAAAAIphA+BJ2vqmoNLLooswswlrx2cIQTmiabjF3lr+ICuPNGXvYHCzpd9XwAAOw==)'
-    }
-  ])
-);
+import '@mercateo/ws/dist/storybook-config';
+import { configure } from '@storybook/react';
 
 configure(() => {
   const req = require.context('../stories', true, /.*\.tsx$/);
